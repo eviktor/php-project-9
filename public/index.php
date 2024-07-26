e6149ac8788c942e87ef7ee48cc2aefb42a963e1
eViktor <vk7771199@gmail.com>
2024-07-26 23:55:47 +0500

    build: Install twig

--- public/index.php
+++ public/index.php
@@ -1,14 +1,35 @@
 <?php
 
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
+// Create Container
+$container = new Container();
 
-$app = AppFactory::create();
+// Set view in Container
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../templates', ['cache' => __DIR__ . '/../cache']);
+});
+
+// Create App from container
+$app = AppFactory::createFromContainer($container);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+// Add other middleware
+$app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
+// Render from template file templates/profile.html.twig
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-});
+    $twig = $this->get(Twig::class);
+    return $twig->render($response, 'index.html.twig');
+})->setName('index');
+
+// Run app
 $app->run();
