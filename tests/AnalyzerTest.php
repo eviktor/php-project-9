f1fd04005dff0d25c4644fd19f81e5a139316c6f
eviktor <vk7771199@gmail.com>
2024-09-24 01:17:41 +0700

    test: Add initial empty test

---
+++ tests/AnalyzerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AnalyzerTest extends TestCase
+{
+    public function testAnalyzer(): void
+    {
+        $this->assertTrue(true);
+    }
+}
