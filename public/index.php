cc7a31245ba93a8ba667d175d11626bd12bb38dc
eviktor <vk7771199@gmail.com>
2024-10-07 01:27:24 +0700

    feat: Add layout for pages, add url page

--- public/index.php
+++ public/index.php
@@ -7,10 +7,8 @@ use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Create Container
 $container = new Container();
 
-// Set view in Container
 $container->set(Twig::class, function () {
     return Twig::create(
         __DIR__ . '/../templates',
@@ -18,21 +16,19 @@ $container->set(Twig::class, function () {
     );
 });
 
-// Create App from container
 $app = AppFactory::createFromContainer($container);
-
-// Add Twig-View Middleware
 $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
-
-// Add other middleware
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-// Render from template file templates/profile.html.twig
 $app->get('/', function ($request, $response) use ($container) {
     $twig = $container->get(Twig::class);
     return $twig->render($response, 'index.html.twig');
 })->setName('index');
 
-// Run app
+$app->get('/urls', function ($request, $response) use ($container) {
+    $twig = $container->get(Twig::class);
+    return $twig->render($response, 'urls.html.twig');
+})->setName('urls');
+
 $app->run();
