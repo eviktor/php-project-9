3aa9c2bdf476b0b1de1388b40a108fa96d48a952
eviktor <vk7771199@gmail.com>
2024-10-16 11:14:17 +0700

    feat: Add Url model

---
+++ app/Models/Url.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+
+class Url
+{
+    private ?int $id = null;
+    private ?string $name = null;
+    private ?Carbon $createdAt = null;
+
+    public static function fromArray(array $urlData): Url
+    {
+        $url = new Url();
+        $url->setName($urlData['name']);
+        if (array_key_exists('created_at', $urlData)) {
+            $url->setCreatedAt(Carbon::parse($urlData['created_at']));
+        }
+
+        return $url;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getCreatedAt(): ?Carbon
+    {
+        return $this->createdAt;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setCreatedAt(?Carbon $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function exists(): bool
+    {
+        return !is_null($this->getId());
+    }
+}
