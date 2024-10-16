bd0710e21b05101b4b22a979790684363ef26422
eviktor <vk7771199@gmail.com>
2024-10-16 11:14:50 +0700

    feat: Add Url repository

---
+++ app/Repositories/UrlRepository.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Url;
+
+class UrlRepository extends Repository
+{
+    public function getEntities(): array
+    {
+        $urls = [];
+        $sql = "SELECT * FROM urls";
+        $stmt = $this->conn->query($sql);
+
+        while ($row = $stmt->fetch()) {
+            $url = Url::fromArray([
+                'name' => $row['name'],
+                'created_at' => $row['created_at']
+            ]);
+            $url->setId($row['id']);
+            $urls[] = $url;
+        }
+
+        return $urls;
+    }
+
+    public function find(int $id): ?Url
+    {
+        $sql = "SELECT * FROM urls WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        if ($row = $stmt->fetch()) {
+            $url = Url::fromArray([$row['name'], $row['created_at']]);
+            $url->setId($row['id']);
+            return $url;
+        }
+
+        return null;
+    }
+
+    public function save(Url $url): void
+    {
+        if ($url->exists()) {
+            $this->update($url);
+        } else {
+            $this->create($url);
+        }
+    }
+
+    private function update(Url $url): void
+    {
+        $sql = "UPDATE urls SET name = :make, created_at = :created_at WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $id = $url->getId();
+        $name = $url->getName();
+        $createdAt = $url->getCreatedAt();
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':created_at', $createdAt);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    private function create(Url $url): void
+    {
+        $sql = "INSERT INTO urls (name) VALUES (:name)";
+        $stmt = $this->conn->prepare($sql);
+        $name = $url->getName();
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+        $id = (int) $this->conn->lastInsertId();
+        $url->setId($id);
+    }
+}
